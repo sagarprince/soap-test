c956b37cbb64bfffcf7bd03f1143b7b3ffe2071a
Alex Dean <alex@crackpot.org>
2011-09-02 13:17:22 -0500

    client accepts args for server url and person's name

--- client.php
+++ client.php
@@ -1,4 +1,16 @@
 <?php
-$client = new SoapClient("hello_world.wsdl", array('location'=>'http://localhost/~alex/soap-test/server.php'));
-echo $client->sayHello("barf");
+/*
+Usage: php client.php <url of server> <a name>
+
+$ php client.php http://localhost/~alex/soap-test/server.php Kelsey
+=> Hello Kelsey!
+*/
+
+if(count($argv) != 3) {
+  echo "Usage: php client.php <url of server> <a name>\n";
+  exit;
+}
+
+$client = new SoapClient( "hello_world.wsdl", array( 'location' => $argv[1] ) );
+echo $client->sayHello( $argv[2] );
 ?>
